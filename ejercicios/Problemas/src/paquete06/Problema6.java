ec1885895f69dc3e5286ec150c7a5c70dce97910
teoperalta <teo.peralta.2007@gmail.com>
2026-01-09 11:45:01 -0500

    Problema6

--- ejercicios/Problemas/src/paquete06/Problema6.java
+++ ejercicios/Problemas/src/paquete06/Problema6.java
@@ -10,5 +10,30 @@ package paquete06;
  * @author reroes
  */
 public class Problema6 {
-    
+     public static void main(String[] args) {
+         int[] informacion = {1, 2, 3, 10, 20, 30, 100, 200, 300};
+         double media;
+         double desviacion;
+         media = obtenerMedia(informacion);
+         desviacion = calcularDesviacion(informacion, media);
+         System.out.printf("La media es: %.2f\nLa desviación es: %.2f\n", media,
+                 desviacion);
+     }
+      public static double obtenerMedia(int[] datos) {
+         double media;
+         double suma = 0;
+         for (int i = 0; i<datos.length;i++){
+             suma= suma + datos[i];
+         }
+         media = suma / datos.length;
+         return media;
+     }
+     
+     public static double calcularDesviacion(int[] arreglo, double media) {
+        double sumaVariaciones = 0;
+        for (int i = 0; i < arreglo.length; i++) {
+        sumaVariaciones += Math.pow(arreglo[i] - media, 2);
+    }
+    return Math.sqrt(sumaVariaciones / arreglo.length);
+}
 }
