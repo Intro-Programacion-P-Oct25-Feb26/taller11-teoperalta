b02e90df28ad4874805d29af2647ab16a7fdfcc9
teoperalta <teo.peralta.2007@gmail.com>
2026-01-09 13:48:00 -0500

    Problema7

--- ejercicios/Problemas/src/paquete07/Problema7.java
+++ ejercicios/Problemas/src/paquete07/Problema7.java
@@ -5,10 +5,40 @@
  */
 package paquete07;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 /**
  *
  * @author reroes
  */
 public class Problema7 {
-    
+     public static void main(String[] args) {
+         String []datos;
+    datos = obtenerCadenas ();
+    valores(datos);
+}
+    public static String []obtenerCadenas (){
+        Scanner entrada = new Scanner(System.in);
+        entrada.useLocale(Locale.US);
+        int n;
+            System.out.println("INGRESE LA CANTIDAD DE ELEMENTOS");
+            n = entrada.nextInt();
+            entrada.nextLine();
+            String[] arreglo= new String [n];
+            for (int i =0; i<arreglo.length;i++){   
+                System.out.println("Ingrese el elemento");
+                String elemento = "";
+                elemento = entrada.nextLine();
+                arreglo[i] = elemento;
+            }
+            return arreglo;
+    }  
+    public static void valores (String [] a){
+        for (int i = 0; i<a.length;i++){
+            if ((a[i].length()==4)||(a[i].length()==5)){
+                System.out.printf("%s\n",a[i]);
+            }
+        }
+    }
 }
